26fbbea882a7d4ba0296b2220cb2d7a330e36bc5
Assaf Itzikson <sysqo82@gmail.com>
2026-01-16 21:31:48 +0000

    Remove unused dependencies and cleanup project
    Phase 1 cleanup:
    - Remove Symfony Mercure bundle (completely unused)
    - Remove Symfony Messenger component (only default config)
    - Remove Nyholm PSR-7 package (unused)
    - Remove unused NPM packages: critical, file-loader, glob-all, react-datalist-input, react-modal
    - Remove empty ApiResource directory
    - Remove mysterious C/ directory
    - Update webpack config to use built-in glob instead of glob-all
    - Remove API Platform (auto-removed as dependency of Mercure)
    
    Results:
    - Removed 39 Composer packages
    - Removed 239 NPM packages
    - Cleaner dependency tree
    - Production build working
    - Dev environment tested and working

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,7 +5,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
